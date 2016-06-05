f694ce30af20238fc3cc861bc7f3f836708cd5ab
Mark Smithies <marksmithies_7@hotmail.com>
2016-06-05 15:51:39 +0100

    created gitignore file and ignored .metadata

--- GCHQChristmasSquares/src/com/smithies/puzzles/gchq/christmassquares/controller/ChristmasSquaresController.java
+++ GCHQChristmasSquares/src/com/smithies/puzzles/gchq/christmassquares/controller/ChristmasSquaresController.java
@@ -1,12 +1,8 @@
 package com.smithies.puzzles.gchq.christmassquares.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import com.smithies.puzzles.gchq.christmassquares.model.AllInitialClues;
 
